f8ce1854136e6fd69207634a4f20919a97a8fa00
krishjan05 <krishjan05@gmail.com>
2016-12-13 00:44:25 -0500

    Added the code to fetch complete list of employee using linq.

---
+++ LinqExample/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Position { get; set; }
+    }
+}
--- LinqExample/Program.cs
+++ LinqExample/Program.cs
@@ -16,6 +16,12 @@ namespace LinqExample
             set { _Items = value; OnPropertyChanged(); }
         }
         private List<Product> _Items;
+        public List<Employee> EmployeeList
+        {
+            get { return _EmployeeList; }
+            set { _EmployeeList = value; OnPropertyChanged(); }
+        }
+        private List<Employee> _EmployeeList;
         public Program()
         {
             var products = new List<Product> {
@@ -25,10 +31,27 @@ namespace LinqExample
                                 new Product {ProductID = 4, ProductName = "fourth vegetable", UnitPrice = (decimal)26.0m },
                                 new Product {ProductID = 5, ProductName = "fifth candy", UnitPrice = (decimal)32.0m },
                                 };
+
+            var employee = new List<Employee> {
+                                new Employee { EmployeeID = 101, EmployeeName = "Krishna Kanhaiya", Position = "Developer" },
+                                new Employee { EmployeeID = 102, EmployeeName = "Harsh Mahale", Position = "iOS Developer" },
+                                new Employee { EmployeeID = 103, EmployeeName = "Gurpreet Singh", Position = "Manager" },
+                                new Employee { EmployeeID = 104, EmployeeName = "Harsimar Ahluwalia", Position = "Developer" },
+                                new Employee { EmployeeID = 105, EmployeeName = "Tanmay Teckchandani", Position = "Developer" },
+                                };
+            
+            var AllProductList = products.Select(p => new {p});
+
             var productCandy = products.Where(p => p.ProductName.Contains("candy"));
             productCandy = productCandy.ToList<Product>();
-            _Items = productCandy.ToList();
-            Items = _Items;
+             _Items = productCandy.ToList();
+             Items = _Items;
+
+            var AllEmployee = from e in employee
+                              select e;
+            AllEmployee = AllEmployee.ToList<Employee>();
+            _EmployeeList = AllEmployee.ToList();
+            AllEmployee = _EmployeeList;
         }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -39,7 +62,5 @@ namespace LinqExample
                 eventHandler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-
-
     }
 }
